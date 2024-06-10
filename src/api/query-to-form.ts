f91a79205efc69cbc83fb98ea4d8fcb89fce82cd
Poteryaev Dmitriy <d.poteryaev@rs.akytec.global>
2024-06-10 16:15:51 +0200

    fix/ add headers

--- src/api/query-to-form.ts
+++ src/api/query-to-form.ts
@@ -18,7 +18,7 @@ const  postInfoFromForm =  async () => {
           "body": "update_cart_action=empty_cart&update_cart_action=empty_cart&isrfq=1",
           "method": "POST",
           "mode": "no-cors",
-          "credentials": "omit"
+          "credentials": "include",
         });
 
 
@@ -46,9 +46,10 @@ const  postInfoFromForm =  async () => {
           },
       
           "body": formData,
+          "referrerPolicy": "strict-origin-when-cross-origin",
           "method": "POST",
           "mode": "no-cors",
-          "credentials": "include"
+          "credentials": "include",
         });
         console.log(res_add);
      
