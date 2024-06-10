a4a6cab2a60cf08337c5ea6bdba194856f6e0089
Poteryaev Dmitriy <d.poteryaev@rs.akytec.global>
2024-06-10 14:25:51 +0200

    feature/ add query to form

--- src/ui/client-header/client-header.tsx
+++ src/ui/client-header/client-header.tsx
@@ -1,6 +1,8 @@
 
 import React, {FC} from 'react';
-import  {Button} from "../buttons/button/button"
+import  {Button} from "../buttons/button/button";
+import {formAPI} from "../../api/query-to-form";
+
 
 import classes from "./styles/client-header.module.scss"; 
 
@@ -8,8 +10,10 @@ import classes from "./styles/client-header.module.scss";
 
 export const ClientHeader: FC = () => {
 
+    const {postInfoFromForm} = formAPI;
+
     return(
-    <header className={classes.client_header}>
+    <header className={classes.client_header} onClick={() => postInfoFromForm()}>
     <div className={classes.client_header_container}>
     <Button variant='magenta' size='medium'>REQUEST FOR QUOTE</Button>
     </div>
