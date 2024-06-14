efcab269459fb446c881f24dcad38e3504ce6481
Poteryaev Dmitriy <d.poteryaev@rs.akytec.global>
2024-06-14 12:07:12 +0200

    feature/ add styles for svgs

---
+++ src/ui/icon2/Mx1102.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+
+const SvgMx1102 = (props: SVGProps<SVGSVGElement>) => (
+
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={71}
+    height={71}
+    fill="none"
+    {...props}
+  >
+    <circle cx={35.848} cy={35.887} r={34.25} stroke="#fff" strokeWidth={1.5} />
+    <circle cx={29.838} cy={29.58} r={15.803} stroke="#fff" strokeWidth={1.5} />
+    <path
+      fill="#fff"
+      d="m38.563 42.547 3.634-3.635L56.594 53.31a2.57 2.57 0 0 1-3.635 3.634z"
+    />
+    <path
+      stroke="#fff"
+      strokeWidth={1.5}
+      d="m14.733 29.583 7.47-5.161a15.68 15.68 0 0 1 15.401-.002l7.277 5.148-7.477 5.369a15.68 15.68 0 0 1-15.389.002z"
+    />
+    <circle cx={29.803} cy={29.682} r={4.463} fill="#fff" />
+  </svg>
+);
+export default SvgMx1102;
