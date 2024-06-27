3f6e2ad5de1d3162047358e6489d8e089d4cd5fd
Poteryaev Dmitriy <d.poteryaev@rs.akytec.global>
2024-06-27 10:22:53 +0200

    feature/ add png

--- src/modules/applications/application2/Application2.tsx
+++ src/modules/applications/application2/Application2.tsx
@@ -55,7 +55,6 @@ export const Application2 = () => {
           </div>
           <figure className={classes.application2_mainImg}>
             <Image alt="main" src="/png/application2/main.png" width={1530} height={1149}/>
-            <Image alt="main" src="/png/application2/main.png" width={1530} height={1149}/>
           </figure>
           <article className={classes.application2_textDescription}>
             <header>
