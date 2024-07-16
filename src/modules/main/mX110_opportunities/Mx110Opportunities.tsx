b6e23ac47d46ae0727d74b7d73bcf04c44392ce9
Poteryaev Dmitriy <d.poteryaev@rs.akytec.global>
2024-07-16 12:09:27 +0300

    fix/ change mx110 opportinities

--- src/modules/main/mX110_opportunities/Mx110Opportunities.tsx
+++ src/modules/main/mX110_opportunities/Mx110Opportunities.tsx
@@ -1,38 +1,44 @@
 import React, { FC } from "react";
-import classes from "./styles/Mx110opportunities.module.scss";
+import classes from "../mX210_opportunities/styles/Mx210opportunities.module.scss";
 import SvgMx210427321481 from "../../../ui/icon2/Mx210427321481";
-import SvgMx210427321717 from "../../../ui/icon2/Mx210427321717";
 import SvgMx110427321542 from "../../../ui/icon2/Mx110427321542";
 import SvgMx110427321769 from "../../../ui/icon2/Mx110427321769";
+import SvgMx210427321717 from "../../../ui/icon2/Mx210427321717";
+import SvgMx2101 from "../../../ui/icon2/Mx2101";
+import Mx1102 from "../../../ui/icon2/Mx1102";
 import { CardOpportuinities } from "../../../ui/card-opportunities/card-opportunities";
-import Image from 'next/image';
+import Image from "next/image";
 
 const cards =
-    [{ topInscription: "Modbus RTU,", bottomInscription: "ASCII", svg: <SvgMx210427321481/> },
-     { topInscription: 'Galvanic',bottomInscription: "isolation", svg: <SvgMx110427321769/>}, 
-     {topInscription: 'Diagnostics of', bottomInscription: 'network status', svg: <SvgMx110427321542/>}, 
-     {topInscription:'Up to 32DI / 32DO', bottomInscription:'Up to 8AI / 8AO', svg: <SvgMx210427321717/>, path: "path"}];
+    [{ topInscription: "Modbus RTU,", bottomInscription: "ASCII", svg: <SvgMx210427321481 /> },
+    { topInscription: 'Galvanic', bottomInscription: "isolation", svg: <SvgMx110427321769 /> },
+    { topInscription: 'Diagnostics of', bottomInscription: "network status", svg: <SvgMx110427321542 /> },
+    { topInscription: 'Up to 32DI / 32DO', bottomInscription: 'Up to 8AI / 6AO', svg: <SvgMx210427321717 /> },
+    { topInscription: 'Wide operating', bottomInscription: 'temperature range', svg: <SvgMx2101 /> },
+    { topInscription: 'Sensor-based', bottomInscription: 'status diagnostics', svg: <Mx1102 /> }];
 
 
 export const Mx110Opportunities: FC = () => {
 
-    return <section className={classes.Mx110opportunities} id='Mx110'>
-        <div className={classes.Mx110opportunities_line}>Mx110</div>
-        <div className={classes.Mx110opportunities_fullInfo}>
-             <div className={classes.Mx110opportunities_wrapperList}>
-                <div style={{width: "100%", display: "flex"}}>
-                <ul className={classes.Mx110opportunities_listOpportunities}>
+    return <section className={classes.Mx210opportunities} id='Mx110'>
+        <div className={classes.Mx210opportunities_line}><div>Mx110 <span>with RS485</span></div></div>
+        <div className={classes.Mx210opportunities_fullInfo}>
+            <article>
+                <p><strong>Mx110</strong> - the line of universal I/O modules with RS485 interfaces offer a cost-effective and flexible 
+                solution for centralized and decentralized systems. The modules are connected to the master via a two-wire 
+                communication line over a distance of up to 1200 meters.</p>
+                <div className={classes.Mx210opportunities_wrapperImg}>
+                    <Image width={535} height={535} className={classes.Mx210opportunities_imgModule} src="/png/main/Oppotunities/Mx110/Mx110.png" alt="Mx110" />
+                    <div>Compatibility with third-party PLCs</div>
+                </div>
+            </article>
+            <div style={{ width: "100%", display: "flex" }}>
+                <ul className={classes.Mx210opportunities_listOpportunities}>
                     {cards.map((card: any) => {
-                        return (<li key={card.topInscription}><CardOpportuinities description={card}/></li>)
+                        return (<li key={card.topInscription}><CardOpportuinities description={card} /></li>)
                     })}
                 </ul>
-                </div>
-            </div> 
-             <div className={classes.Mx110opportunities_wrapperImg}>
-             <Image className={classes.Mx110opportunities_imgModule} width={626} height={626} src='/png/main/Oppotunities/Mx110/Mx110.png' alt="Mx210" />
-             <div>Compatibility with third-party PLCs</div>
-             </div>
-         
+            </div>
         </div>
     </section>
 }
