d5d7dba84f08309e635e744232c9307b1b12dfd7
Poteryaev Dmitriy <d.poteryaev@rs.akytec.global>
2024-07-16 13:49:52 +0300

    fix/ change styles Mx110 swiper block

--- src/modules/main/Mx110_swiperBlock/Mx110_swiperBlock.tsx
+++ src/modules/main/Mx110_swiperBlock/Mx110_swiperBlock.tsx
@@ -1,34 +1,38 @@
 import React from "react";
 import { CustomSwiper } from "../../../ui/custom-swiper/custom-swiper";
 import classes from "./style/swiper-block.module.scss";
-        
-const pngsProtocolsMx110 = [
+import { Button } from "../../../ui/buttons/button/button";
+import Link from "next/link";
+
+const pngsProtocolsMx210 = [
     {url: '/png/main/multiple-protocols/Mx110/Mx110_protocols_1.svg',center: false},
-    {url: '/png/main/multiple-protocols/Mx110/Mx110_protocols_2.svg',center: false},
-    {url: '/png/main/multiple-protocols/Mx110/Mx110_protocols_3.svg',center: false} ];
+    {url: '/png/main/multiple-protocols/Mx110/Mx110_protocols_2.svg',center: true},
+    {url: '/png/main/multiple-protocols/Mx110/Mx110_protocols_3.svg',center: false}, ];
+
 
 export const Mx110_swiperBlock = () => {
 
     return (
-        <section className={classes.swiper_block}>
-            <article>
-                <header>Multiple Protocols</header>
-                <p>Universal Modbus RTU I/O modules Mx110 offer cost effective and flexible solution for centralized and decentralized systems.
-                    The modules are connected to the Master using a two-wire communication line up to 1200 m.
-                </p>
-                <header>The master can be a device of various types:</header>
+       <section className={classes.swiper_block}>
+        <div className={classes.swiper_wrapperBlock}>
+        <article>
+            <header>Connection options</header>
             <ul>
-                <li>Third-party PLCs with RS485;</li>
-                <li>PC with SCADA via IC4-M (converter RS485{"->"}USB);</li>
-                <li>CLOUD services;</li>
-                <li>Modbus display SMI2-M;</li>
-                <li>Operator panels, etc.</li>
+                <li>Increase the number of inputs and outputs of any PLC with RS485 in centralized and decentralized systems.</li>
+                <li>Connecting modules to cloud services through routers or gateways for remote monitoring and control.</li>
+                <li>Connecting to a PC for remote data acquisition into a SCADA system or other software.</li>
+                <li>Compatibility with any equipment supporting the RS-485 interface and Modbus-RTU/ASCII protocols.</li>
             </ul>
-            </article>
-          
-            <div style={{ maxWidth: '620px', width: '100%' }}>
-                <CustomSwiper arrayWithPng={pngsProtocolsMx110} />
-            </div>
-        </section>)
+            <div style={{marginTop: '69px'}}>
+            <Button variant='magenta' size='medium' >                   
+                        <Link style={{ width: "100%", height: "100%", display: 'flex', justifyContent: 'center', alignItems: 'center' }} href='/'>
+                        EXPLORE MODELS {"&"} SPECS
+                    </Link> </Button></div>
+        </article>
+        <div style={{ maxWidth: '620px', width: '100%' }}>
+        <CustomSwiper arrayWithPng={pngsProtocolsMx210}/>
+        </div>
+        </div>
+       </section>)
 
 }
