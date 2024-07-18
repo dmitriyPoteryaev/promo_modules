a8a8314c80a20b1523d4a83a9afce2d62c0352d7
Poteryaev Dmitriy <d.poteryaev@rs.akytec.global>
2024-07-18 15:45:36 +0300

    feature/ add ad block

--- src/modules/main/Main.tsx
+++ src/modules/main/Main.tsx
@@ -11,7 +11,7 @@ import {SwiperBlock} from "./swiper-block/swiper-block";
 import { EasyCommunication } from "./easy-communication/easy-communication";
 import { Modifications_Mx210 } from "./modifications_Mx210/Modifications_Mx210";
 import { Modifications_Mx110 } from "./modifications_Mx110/modifications_Mx110";
-import {AdAkytec} from "./ad-akytec/ad-akytec";
+import {AdAkytec, AdAkytecBottom} from "./ad-akytec/ad-akytec";
 import { TableComprasion } from "./module-comprasion-table/module-comprssion-table";
 
 export const Main = () => {
@@ -32,6 +32,7 @@ export const Main = () => {
      <TableComprasion/>
      <Modifications_Mx210/>
      <Modifications_Mx110/>
+     <AdAkytecBottom/>
     </ClientLayout>
   );
 };
